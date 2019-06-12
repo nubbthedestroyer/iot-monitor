81f1e5979a174b20852516743db72cf9830c7e5f
nubbthedestroyer <michael@desktopninjas.com>
2019-06-12 01:47:46 -0500

    init change

--- install-monitor.sh
+++ install-monitor.sh
@@ -5,6 +5,7 @@ sudo rpm -U ./amazon-cloudwatch-agent.rpm
 
 cat config-file-template.json | "s/container-test-1/${1}/g" > config-file.json
 
+
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m onPremise \
