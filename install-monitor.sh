7f9c6b6a3b4a26c336f3f9b27cecad3aeb4ba218
nubbthedestroyer <michael@desktopninjas.com>
2019-06-12 01:28:23 -0500

    Initial commit

---
+++ install-monitor.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+wget -q https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
+
+mkdir -p ~/.aws
+cp credentials ~/.aws/credentials
+chmod 600 ~/.aws/credentials
+
+cat config-file-template.json | "s/container-test-1/${1}/g" > config-file.json
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m onPremise \
+    -c file:`pwd`/config-file.json \
+    -s
