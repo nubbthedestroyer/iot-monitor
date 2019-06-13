0159d804376253fb3fbb52daa2479f15307c3e56
nubbthedestroyer <michael@desktopninjas.com>
2019-06-13 12:03:50 -0500

    updated to support ubuntu instead of cent, added common-config change for custom credential location

--- install-monitor.sh
+++ install-monitor.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-wget -q https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
-sudo rpm -U ./amazon-cloudwatch-agent.rpm
+wget -q https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -E -i ./amazon-cloudwatch-agent.rpm
 
 cat config-file-template.json | "s/container-test-1/${1}/g" > config-file.json
 
+echo "[credentials]
+    shared_credential_file = \"/opt/aws/amazon-cloudwatch-agent/etc/\"
+" > /opt/aws/amazon-cloudwatch-agent/etc/common-config.toml
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
