10acfaaf855ff1a238f9cfb38101fd2a27e73c60
nubbthedestroyer <michael@desktopninjas.com>
2019-06-14 08:25:43 -0500

    added credentials

--- install-monitor.sh
+++ install-monitor.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 wget -q https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -E -i ./amazon-cloudwatch-agent.rpm
 
 cat config-file-template.json | "s/container-test-1/${1}/g" > /opt/aws/amazon-cloudwatch-agent/etc/config-file.json
+
+cp credentials /opt/aws/amazon-cloudwatch-agent/etc/
 
 echo "[credentials]
     shared_credential_file = \"/opt/aws/amazon-cloudwatch-agent/etc/\"
